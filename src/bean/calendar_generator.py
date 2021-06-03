377887f1177b6715bccb0e76701ea4bec7442126
Dennis <datm2727@gmail.com>
2021-06-03 00:14:48 -0500

    Add files via upload

--- src/bean/calendar_generator.py
+++ src/bean/calendar_generator.py
@@ -5,17 +5,20 @@ from io import BytesIO
 
 font_title = ImageFont.truetype('src/fonts/KGPrimaryWhimsy.ttf', 130)
 font_day_number = ImageFont.truetype('src/fonts/KGPrimaryWhimsy.ttf', 50)
+font_day_str = ImageFont.truetype('src/fonts/KGPrimaryWhimsy.ttf', 90)
 
 SQUARE_DAY_SIZE = (128, 128)
-W, H = (1056, 1050)
+W, H = (1056, 824)
 
 title_color = (255, 255, 0)
 
 img_day_weekend_true = Image.new('RGB', SQUARE_DAY_SIZE, color = (135, 184, 205))
 img_day_weekend_false = Image.new('RGB', SQUARE_DAY_SIZE, color = (11, 161, 112))
 
+zone_day_str_y = 150
+
 zone_days_x_init = 50
-zone_days_y_init = 200
+zone_days_y_init = 250
 
 month_str = {
     1: 'Enero',
@@ -32,22 +35,46 @@ month_str = {
     12: 'Diciembre'
 }
 
+day_str = {
+    0: 'D',
+    1: 'L',
+    2: 'M',
+    3: 'M',
+    4: 'J',
+    5: 'V',
+    6: 'S'
+}
+
 class CalendarGenerator():
     def new_calendar_img(self, month_int, bytes_dict):
-        img_base = Image.new('RGB', (W, H), color = (62, 45, 82))
+        day_relative_base = datetime.datetime(2021, month_int, 1).weekday()
+        a, total_days = calendar.monthrange(2021, month_int)
+        max_day_num = day_relative_base + total_days
+        y = int(max_day_num / 7)
+
+        x_inc = 138
+        y_inc = 138
+        
+        img_base = Image.new('RGB', (W, H+y_inc*(y-3)), color = (62, 45, 82))
         draw = ImageDraw.Draw(img_base)
 
         text_title = f"{month_str[month_int]} - 2021"
 
         w, h = draw.textsize(text_title, font=font_title)
-        draw.text(((W-w)/2, 50), text_title, font=font_title, fill=(255, 255, 0))
+        draw.text(((W-w)/2, 10), text_title, font=font_title, fill=(255, 255, 0))
 
-        day_relative_base = datetime.datetime(2021, month_int, 1).weekday()
+        
 
-        x_inc = 138
-        y_inc = 138
+        
 
-        a, total_days = calendar.monthrange(2021, month_int)
+        for i in range(7):
+            day_name = day_str[i]
+
+            w_d, h_d = draw.textsize(day_name, font=font_day_str)
+
+            pos_day_name_x = zone_days_x_init + i * x_inc + x_inc/2 - w_d/2
+
+            draw.text((pos_day_name_x, zone_day_str_y), day_name, font=font_day_str, fill=(255, 165, 0))
 
         for num_day in range(1, total_days+1):
             day_datetime = datetime.datetime(2021, month_int, num_day)
