cf89d559be524eb684f1f124303678cb33f0bcbd
LawLLM <74462672+LawLLM@users.noreply.github.com>
2021-11-05 18:17:18 +0100

    Update useful_cog.py

--- src/cogs/useful_cog.py
+++ src/cogs/useful_cog.py
@@ -12,11 +12,15 @@ class UsefulCog(commands.Cog):
 		self.bot = bot
 		
 	@commands.command(aliases=['yt'])
-	async def ytsearch(self, ctx, *, search):
-		query_string = parse.urlencode({'search_query': search})
-		html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
-		search_results = re.findall(r'/watch\?v=(.{11})', html_content.read().decode())
-		await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
+	async def ytsearch(self, ctx):
+		search = " ".join(ctx.message.content.split()[1:])
+		if search != "":
+			query_string = parse.urlencode({'search_query': search})
+			html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+			search_results = re.findall(r'/watch\?v=(.{11})', html_content.read().decode())
+			await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
+		else:
+			await ctx.send("Uso correcto: la!yt <titulo>")
 		
 		
 def setup(bot):
